a90729213c2f6f9997be349f51511e514aade3f8
GerardoMontelon <gmontelon@ucol.mx>
2021-09-26 13:15:29 -0500

    Tarea 2 hecha
    Realización de la segunda tarea

---
+++ 1p/2complexity/Tarea2GMR.js
@@ -0,0 +1,21 @@
+var c = 200;
+var j = 2;
+var numeroale=15;
+var numerosPrimos = [2,3,5,7];
+
+for (; j < c; j++) {
+
+  if (j % 2 === 0 || j%3===0 || j%5===0 || j%7===0) {
+    
+  }else{
+    numerosPrimos.push(j);
+  }
+  
+}
+console.log("Numero Primos")
+console.log(numerosPrimos);
+
+
+console.log("Numero Aleatorio");
+console.log( Math.floor(Math.random() * numeroale));
+
