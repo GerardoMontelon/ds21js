d2cb97d096759b4725a23739a6d372e99a0777d6
GerardoMontelon <gmontelon@ucol.mx>
2021-09-26 14:17:05 -0500

    Tarea 3 hecha
    La terea numero 3 a sido realizada

---
+++ 1p/3vector/tarea3GMR.js
@@ -0,0 +1,16 @@
+//Crea un array
+var Array = [];
+console.log("Prueba antes de llenar el vector");
+console.log(Array);
+Array[0]=20;
+console.log("Prueba despues del llenado")
+console.log(Array);
+Array[1]=43;
+console.log("Segunda pruba");
+console.log(Array);
+Array.push(50,20,30,85,100);
+console.log("Numeros Agregados con el push prueba");
+console.log(Array);
+Array.splice(1,3)
+console.log("Borrar las pociciones 1,2 y 3 ")
+console.log(Array)
